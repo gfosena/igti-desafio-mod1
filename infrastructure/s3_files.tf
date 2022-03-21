0fa846f72a87230d9cb6d76f924bcd95738e6d69
Gustavo Sena <gfosena@gmail.com>
2022-03-21 12:35:38 -0300

    ajusta nome bucket

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "job_spark" {
   # bucket = aws_s3_bucket.datalake.id
-  bucket = aws_s3_bucket.dl.id
+  bucket = aws_s3_bucket.datalake.id
 
   key    = "emr-code/pyspark/job_spark_rais.py"
   acl    = "private"
