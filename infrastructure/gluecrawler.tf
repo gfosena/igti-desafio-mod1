afe46549c26df4b3dd99fb2e7b7458ffd6331809
Gustavo Sena <gfosena@gmail.com>
2022-03-21 12:38:45 -0300

    ajuste nome var role glue

--- infrastructure/gluecrawler.tf
+++ infrastructure/gluecrawler.tf
@@ -5,7 +5,7 @@ resource "aws_glue_catalog_database" "rais" {
 resource "aws_glue_crawler" "rais" {
   database_name = aws_glue_catalog_database.rais.name
   name          = "rais_s3_crawler"
-  role          = aws_iam_role.glue_rais_role.arn
+  role          = aws_iam_role.glue_role.arn
 
   s3_target {
     path = "s3://${aws_s3_bucket.datalake.bucket}/staging-zone/rais/"
